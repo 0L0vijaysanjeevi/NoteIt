e0d0ece29820c2b08863dfc33047358ead02bfb4
0L0vijaysanjeevi <0.0vijaysanjeevi@gmail.com>
2026-02-11 03:42:53 +0530

    v1

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
+let isitlink=false
 
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
@@ -12,13 +13,29 @@ inputBtn.addEventListener("click", function() {
 function renderLeads() {
     let listItems = ""
     for (let i = 0; i < myLeads.length; i++) {
-        listItems += `
+        isitlink=validateLink(myLeads[i])
+        if (isitlink === true){
+            listItems += `
             <li>
                 <a target='_blank' href='${myLeads[i]}'>
                     ${myLeads[i]}
                 </a>
             </li>
         `
+        }
+        else{
+            listItems += `
+            <li>
+                ${myLeads[i]}
+            </li>
+        `
+        }
     }
     ulEl.innerHTML = listItems  
 }
+function validateLink(inputValue) {
+    const httpRegex = /^(http:\/\/|https:\/\/)/i; 
+    const tldRegex = /\.[a-z]{2,}$/i; 
+    return (httpRegex.test(inputValue) && tldRegex.test(inputValue)) || tldRegex.test(inputValue);
+}
+
